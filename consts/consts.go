b326f0abef907aeea470f3420dd277f7eb391834
jackokring <jackokring@gmail.com>
2024-03-04 21:17:44 +0000

    package naming

--- consts/consts.go
+++ consts/consts.go
@@ -9,3 +9,6 @@ const AppName = "goali"
 
 // The application description.
 const AppDescription = "The " + AppName + " ball saving all in one app."
+
+// The date time format string.
+const DateTimeFormat = "2006-01-02 Mon 15:04:05 MST"
