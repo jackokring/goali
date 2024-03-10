530afa0f521897e18562c2f566655b9fbfd46149
jackokring <jackokring@gmail.com>
2024-03-10 22:52:00 +0000

    Sunday 2024-03-10 22:52:00

--- snake/snake.py
+++ snake/snake.py
@@ -1,6 +1,36 @@
-'''The imported snake python template with mypy stub definitions and any needed wrappers.'''
+"""The imported snake python template with mypy stub definitions and any needed wrappers."""
+from array import ArrayType as array
+import sys
+from io import StringIO, BytesIO
+from typing import Union, Any
+from ctypes import _CData
+from pickle import PickleBuffer
+from mmap import mmap
+
+ByteAlias = Union[bytes, Union[bytearray, memoryview, array[Any], mmap, _CData, PickleBuffer]]
 
 # stdio redirect logic to goali streams
+class ByteOut(BytesIO):
+    def write(self, bytes: ByteAlias) -> int:
+        return 0
+    
+class StdOut(StringIO):
+    def write(self, string: str) -> int:
+        return 0
+    buffer = ByteOut()
+
+sys.stdout = StdOut()
+
+class ByteErr(BytesIO):
+    def write(self, bytes: ByteAlias) -> int:
+        return 0
+
+class StdErr(StringIO):
+    def write(self, string: str) -> int:
+        return 0
+    buffer = ByteErr()
+
+sys.stderr = StdErr()
 
 # stubs replaced by goali but present for mypy syntax and type checks
 
