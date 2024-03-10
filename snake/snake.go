b9b377ab5a78b5068ec090dcc71de73e8b5c8ee5
jackokring <jackokring@gmail.com>
2024-03-10 19:38:31 +0000

    Sunday 2024-03-10 19:38:31

--- snake/snake.go
+++ snake/snake.go
@@ -3,3 +3,17 @@ package snake
 //=====================================
 //********* Embedded Python ***********
 //=====================================
+
+import (
+	"os"
+	"strconv"
+
+	py "github.com/jackokring/cpy3"
+)
+
+func SetIO() {
+	// assume consistency of process file descriptors
+	py.PyRun_SimpleString("sys.stdin = os.fdopen(" + strconv.Itoa(int(os.Stdin.Fd())) + ")")
+	py.PyRun_SimpleString("sys.stout = os.fdopen(" + strconv.Itoa(int(os.Stdout.Fd())) + ")")
+	py.PyRun_SimpleString("sys.stderr = os.fdopen(" + strconv.Itoa(int(os.Stderr.Fd())) + ")")
+}
