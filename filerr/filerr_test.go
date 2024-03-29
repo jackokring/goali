a1553ae096d0fc2d7746cd65e7c92a5e8a16df92
jackokring <jackokring@gmail.com>
2024-03-29 12:08:57 +0000

    Friday 2024-03-29 12:08:54

---
+++ filerr/filerr_test.go
@@ -0,0 +1,16 @@
+package filerr
+
+import (
+	// "regexp"
+	"testing"
+	// "github.com/jackokring/goali/consts"
+)
+
+// A test for package filerr
+//
+// @todo no tests made yet seems like a good one
+// @body checking the todo issue system and wondering about
+// debug levels. It's a const ()?
+func TestFilerr(t *testing.T) {
+
+}
---
+++ pkg/goali_test.go
@@ -0,0 +1,12 @@
+package goali
+
+import (
+	// "regexp"
+	"testing"
+	// "github.com/jackokring/goali/consts"
+)
+
+// A test for package goali
+func TestGoali(t *testing.T) {
+	// no tests yet
+}
