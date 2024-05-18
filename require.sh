fbc6fbcd054bcacc43dd133df2e0e1085e2bfdb4
jackokring <jackokring@gmail.com>
2024-05-18 22:36:19 +0000

    Saturday 2024-05-18 22:36:18

--- require.sh
+++ require.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 #sudo apt install git python-is-python3 golang python3-pip python3-dev postgressql
+# possibly use godeb for more updated go install
+#go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 git pull
-git submodule updsate --init --recursive
+git submodule update --init --recursive
 python -m venv .
 pip install -r requirements.txt
+sqlc generate || echo go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 source gob.sh
