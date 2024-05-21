e45bbbe84f8ec01e489ba77dd3546bd01e885281
jackokring <jackokring@gmail.com>
2024-05-21 17:54:50 +0000

    Tuesday 2024-05-21 17:54:50

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
-CREATE TABLE author (
-  id   BIGSERIAL PRIMARY KEY,
-  name text      NOT NULL,
-  bio  text
+CREATE TABLE item (
+    id          BIGSERIAL PRIMARY KEY,
+    name        TEXT NOT NULL
+    --
+);
+
+CREATE TABLE item_located (
+    item        BIGSERIAL PRIMARY KEY,
+    located     BIGSERIAL NOT NULL
+    --
+);
+
+CREATE TABLE locate_abstract (
+    id          BIGSERIAL PRIMARY KEY,
+    locate_kind BIGSERIAL NOT NULL,
+    locate_real BIGSERIAL NOT NULL
+    --
 );
