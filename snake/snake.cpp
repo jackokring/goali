b46e41fa51114b55d506cc605f27e416ffbf3f67
jackokring <jackokring@gmail.com>
2024-06-07 17:45:09 +0000

    Friday 2024-06-07 17:45:09

---
+++ snake/snake.cpp
@@ -0,0 +1,2 @@
+// for C++ functions (need to be static outside class for `extern "C" {}`)
+#include "snake.hpp"
---
+++ snake/snake.hpp
@@ -0,0 +1,8 @@
+// for C style functions calling C++ (example)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
