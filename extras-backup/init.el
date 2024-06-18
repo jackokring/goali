c8a8e20de191b978c679b8b9eec94d470d476142
jackokring <jackokring@gmail.com>
2024-06-18 16:15:02 +0000

    Tuesday 2024-06-18 16:15:02

--- extras-backup/init.el
+++ extras-backup/init.el
@@ -2,18 +2,49 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-; highlights max
+;; defaults
 (setq font-lock-maximum-decoration t)
+(show-paren-mode 1)
+(cua-mode t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cua-mode t nil (cua-base)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Define C-/ to comment and uncomment regions and lines
+(defun comment-or-uncomment-line-or-region ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)
+        (next-line)))
+(global-set-key (kbd "C-/") 'comment-or-uncomment-line-or-region)
+;; C-a marks the whole buffer
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+
+;; just mine (b)ack, (p)revious, (n)ext reusable cursors?
+
+; n map
+(define-prefix-command 'custom-n-map)
+(global-set-key (kbd "C-n") 'custom-n-map)
+
+; p map
+(define-prefix-command 'custom-p-map)
+(global-set-key (kbd "C-p") 'custom-p-map)
+
+; extend p as right hand extender (user (p)rogramming)
+(define-key custom-p-map (kbd "C-a") 'mark-whole-buffer) ; Select-all.
+(define-key custom-p-map (kbd "C-v") 'yank) ; Paste.
+(define-key custom-p-map (kbd "C-x") 'kill-region) ; Cut.
+(define-key custom-p-map (kbd "C-c") 'kill-ring-save) ; Copy.
+
+; b map
+(define-prefix-command 'custom-b-map)
+(global-set-key (kbd "C-b") 'custom-b-map)
+
+;; my remap of search and save (f)orward -> (f)ind
+; find
+(global-set-key (kbd "C-f") 'isearch-forward)
+; save
+(global-set-key (kbd "C-s") 'save-buffer)
+; quit
+(global-set-key (kbd "C-q") 'save-buffers-kill-terminal)
