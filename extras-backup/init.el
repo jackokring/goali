8210bd6e90cad01d6b16802921e74fdf4b0c1367
jackokring <jackokring@gmail.com>
2024-06-19 18:03:25 +0000

    C-\ custom-escape-map via C-b \ terminal

--- extras-backup/init.el
+++ extras-backup/init.el
@@ -9,6 +9,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
+; C- and ^ are used interchangably as the control key modifier throughout this file
+
 ;; Sensible modern defaults
 (setq font-lock-maximum-decoration t)
 (show-paren-mode 1)
@@ -43,39 +45,49 @@
 ; My ^b map (b)ack -> (b)old -> (b)e
 ; This is not "user" commands, but modified CUA easy extensions
 (define-prefix-command 'custom-b-map)
+; Seems escape is strange as a first character
+(define-prefix-command 'custom-escape-map)
+; C-x = system, C-c = user "letters only", C-\ = as a prefix, it's got previous backing
+(global-set-key (kbd "C-\\") 'custom-escape-map)
 ; technically all C-c should be user defined, but prefix C-b
-; (define-prefix-command 'custom-c-map)
+; B, X and C are handled different as a SIGINT, plus 2 passthroughs
+; for Bad Buffer, eXecute and Command after C-b
+; the rest are for easy intercept requirements
 (global-set-key (kbd "C-b") 'custom-b-map)
-(global-set-key (kbd "C-w") 'move-beginning-of-line) ; WE remap
+(global-set-key (kbd "C-w") 'move-beginning-of-line) ; WE remap beginning/end of line
 (global-set-key (kbd "C-y") (kbd "C-M-c")) ; Y combinator exit recursion remap
-(global-set-key (kbd "C-S-c") (kbd "C-b c")) ; use shift pass thru
 ; Extend my custom-b-map ... tmux shortcut key too
 ; ^ terminals via stty -a => c \ u d q s z r w v o
 ; Those control keys might not be possible to feed into Emacs
 ; as terminal may filter them, so use ^B prefix
 (define-key custom-b-map (kbd "c") mode-specific-map) ; as C-c is copy
 (define-key custom-b-map (kbd "x") ctl-x-map) ; execute as C-x is cut and no '
-(define-key custom-b-map (kbd "\\") (kbd "C-\\")) ; C mode line endings
+; terminal catch extras beyond C-c SIGINT -> simpler terminal requirement
+(define-key custom-b-map (kbd "\\") 'custom-escape-map) ; C mode line endings appears like only found C-x C-\
 (define-key custom-b-map (kbd "u") (kbd "C-u")) ; universal-argument
 (define-key custom-b-map (kbd "d") (kbd "C-d")) ; cursor delete right
 (define-key custom-b-map (kbd "q") (kbd "C-q")) ; quit
 (define-key custom-b-map (kbd "s") (kbd "C-s")) ; save
 (define-key custom-b-map (kbd "z") (kbd "C-z")) ; undo
 (define-key custom-b-map (kbd "r") (kbd "C-r")) ; recursive edit, exit by C-M-c
-(define-key custom-b-map (kbd "w") (kbd "C-w")) ; REMAP of C-a, copy? C-y is paste too
+(define-key custom-b-map (kbd "w") (kbd "C-w")) ; REMAP of C-a beginning of line, copy? C-y is paste too
 (define-key custom-b-map (kbd "v") (kbd "C-v")) ; paste
 (define-key custom-b-map (kbd "o") (kbd "C-o")) ; open line
 ; encourage recursive edits by making exit easier?
-(define-key custom-b-map (kbd "y") (kbd "C-M-c")) ; end recursive edit, Y combinator hint
+(define-key custom-b-map (kbd "y") (kbd "C-y")) ; end recursive edit, Y combinator hint
 ; a few extra conveiniences
 (define-key custom-b-map (kbd "b") 'kill-buffer) ; C-b b kill "bad" buffer (bibi gun)
 ; no ijlm circling the K-ill to end of line
+;; End of the B map
 
 ;; User custom-c-map ^C usually but adapted for "user" commands (^B c)
-; mode-specific-map replaces custom-c-map for C-c
-; help-map for C-h
+; N.B. "mode-specific-map" replaces custom-c-map for C-c
+; also help-map for C-h
 ; It's the common commands of the global mode-specific-map set/unset
 
+
+;; Custom escape map ^\ seems open for use too
+; apparently terminal mode does not pass C-\ through
 
 ;; my remaps for ^f, ^s and ^q
 ; find (f)orward -> (f)ind
@@ -86,11 +98,11 @@
 (global-set-key (kbd "C-q") 'save-buffers-kill-terminal)
 
 ;; buffer navigation sometimes intercepted
-; left/right sometimes won't work
+; left/right sometimes won't work (depends on buffer modes)
 (global-set-key (kbd "M-<up>") 'previous-buffer)
 (global-set-key (kbd "M-<down>") 'next-buffer)
 
-;; with shift <up> is like backspace, <left> is like tab
+;; with shift <up> is like backspace, <left> is like tab (key pointing arrows)
 ; useful for managing window panes
 (global-set-key (kbd "M-S-<left>") 'other-window)
 (global-set-key (kbd "M-S-<right>") 'split-window-right)
