6b5a17ea9bb404bbd29a295569494bec9f64b6c7
jackokring <jackokring@gmail.com>
2024-07-08 20:14:54 +0000

    Monday 2024-07-08 20:14:54

--- xdg.sh
+++ xdg.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 # Source this to set XDG variables
-# set config base
+# set config base plus ...
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 # Import all XDG set by xdg-config-update autorun
 # THey're not exporting by default
 . $XDG_CONFIG_HOME/user-dirs.dirs
