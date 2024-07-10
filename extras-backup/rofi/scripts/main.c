c5f05569c51d435f7ce8e1964f3dad96112696e2
jackokring <jackokring@gmail.com>
2024-07-10 04:05:35 +0000

    Wednesday 2024-07-10 04:05:35

--- extras-backup/rofi/scripts/main.c
+++ extras-backup/rofi/scripts/main.c
@@ -3,35 +3,54 @@
 #include <string.h>
 // FILE* popen("...","w");//?
 char* names[] = {
-	"Compile Rofi Boot",//0
-	"test"//1
+	"Compile a.out via a Bootstrap",//0
+	"Help"//1
 };
 
-void compile(int argc, char** argv) {
-	system("bash -c \"pushd ~/.config/rofi/scripts && gcc main.c && popd\"");// remake a.out
+void back(char sys[]) {
+	// must be static to prevent stack smashing
+	static char bash[256] = "bash -c \"";
+	strcat(bash, sys);
+	strcat(bash, "\" >/dev/null");
+	system(bash);
 }
 
-void nop(int argc, char** argv) { // null function
-	printf("Ah, and a Hello Galaxy from the test function.");
+void compile(int argc, char** argv) {
+	back("pushd ~/.config/rofi/scripts && gcc main.c && popd");// remake a.out
+}
+
+void help(int argc, char** argv) { // help function
+	back("rofi -e 'Ah, and a Hello Galaxy from the help function.'");
 }
 
 void (*(fn[]))(int, char**) = {
 	compile,//0
-	nop//1
+	help//1
+};
+
+char* icon_names[] = {
+	"terminal",//0
+	"help"//1
+};
+
+int icons[] = {
+	0,//0
+	1//1
 };
 
 int main(int argc, char** argv) {
-	--argc;
 	for(int i = 0; i < (sizeof(names)/sizeof(char*)); i++) {
-		switch(argc) {
+		switch(argc - 1) {
 		case 0:// list
-			printf("%s\n", names[i]);
+			printf("%s", names[i]);
+			putc(0, stdout);
+			printf("icon\x1f%s\n", icon_names[icons[i]]);
 			break;
 		default:// process
 			if(strcmp(argv[1], names[i])) break;
-			fn[i](--argc, ++argv);// do it
+			fn[i](argc - 1, argv + 1);// do it
 			return 0; //ok done
 		}
 	}
-	return argc; //ok or not
+	return argc - 1; //ok or not
 }
