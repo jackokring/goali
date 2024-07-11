3fd21476d48bdc60bd56d515e3ff8d8b73b50502
jackokring <jackokring@gmail.com>
2024-07-11 22:54:13 +0000

    Thursday 2024-07-11 22:54:13

--- extras-backup/rofi/scripts/chex.sh
+++ extras-backup/rofi/scripts/chex.sh
@@ -3,4 +3,11 @@ for i in *.sh
 do
 	chmod +x "$i"
 done
-
+# build rofi a(.out) script plugin
+# this will appear in the -modi-combi "a" mode
+# can add C routines just by adapting
+# int main() of sokme other source
+# making the command arrays use it renamed
+# it's capable of compiling itself
+# search "compile" in rofi search
+gcc main.c
