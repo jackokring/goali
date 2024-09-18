70f05e5f0ecfe8d7751dfee0a2c34cf967ae527a
jackokring <jackokring@gmail.com>
2024-09-18 03:38:25 +0000

    Wednesday 2024-09-18 03:38:24

--- extras-backup/nvim/lua/config/keymaps.lua
+++ extras-backup/nvim/lua/config/keymaps.lua
@@ -6,30 +6,52 @@
 -- Some various shifts possibly free
 -- Alt sends an escape prefix
 
-local function nkey(seq, action)
-  vim.keymap.set({ "n", "v" }, seq, action)
+local function iwrap(func)
+  if vim.mode(0) ~= "i" then
+    vim.call(func, nil)
+    return
+  else
+    -- stop insert mode for a bit
+    local cur = vim.getcursorcharpos()
+    vim.cmd.stopinsert()
+    vim.call(func, nil)
+    vim.setpos(cur)
+    -- restore, ah yes, the end of line
+    vim.cmd.startinsert()
+  end
 end
 
-local function nikey(seq, action)
-  vim.keymap.set({ "n", "v", "i" }, seq, action)
+local function nkey(seq, desc, action)
+  vim.keymap.set({ "n", "v" }, seq, action, { desc = desc })
+end
+
+-- action must be a function for this one
+local function nikey(seq, desc, action)
+  vim.keymap.set({ "n", "v", "i" }, seq, function()
+    iwrap(action)
+  end, { desc = desc })
 end
 
 -- Bare Sparse Escape (Not in use)
 -- abcdefghijklmnopqrstuvwxyz
 -- ABCDEFGHIJKLMNOPQRSTUVWXYZ
-nkey("\\a", "")
+nkey("\\a", "", "")
 
 -- Leader Space (Many used, see use pressing <space> in normal mode)
 -- adhijkmnoprtvyz
 -- ABCFGHIJMNOPQRSTUVWXYZ
-nkey("<Leader>a", "")
+nkey("<Leader>a", "", "")
 
 -- Control (Lowercase RESERVED for plugins with no control, uppercase free with no control but shifted)
 -- Can be in insert mode sometimes as control and not <c-v> literal prefixed
 -- ABCDEFHIJKLMOPQRSTUVWXYZ (with control as easiest to finger)
 -- Perculiar shift combination needed
-nikey("<C-_>", "")
+nikey("<C-_>", "", function()
+  -- nil
+end)
 -- NOT <C-N> or <C-G> but rest of controls and not lowercase
-nikey("<C-\\><C-A>", "")
+nikey("<C-\\><C-A>", "", function()
+  -- nil
+end)
 -- Uppercase with shift
-nkey("<C-\\><S-A>", "")
+nkey("<C-\\><S-A>", "", "")
