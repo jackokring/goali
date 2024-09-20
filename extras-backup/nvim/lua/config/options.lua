f17195da7e8b77b8c7eaf4be607255d797bf871c
jackokring <jackokring@gmail.com>
2024-09-20 15:06:24 +0000

    OK,

--- extras-backup/nvim/lua/config/options.lua
+++ extras-backup/nvim/lua/config/options.lua
@@ -3,6 +3,10 @@
 -- Add any additional options here
 
 -- column line of 81 to set limit line
-vim.api.nvim_set_option_value("colorcolumn", "81", {})
+vim.o.colorcolumn = "81"
 -- vim.opt.colorcolumn = 80
-vim.opt.timeoutlen = 500
+vim.o.timeoutlen = 500
+-- for key sequence determinacy
+vim.o.scrolloff = 16
+-- kept screen cursor in centre when scrolling
+-- that really help when expanding the which key help
