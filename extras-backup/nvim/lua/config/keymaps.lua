de515f8466c110fd17139d3acc8da5803c2b84aa
jackokring <jackokring@gmail.com>
2024-09-20 16:33:20 +0000

    OK,

--- extras-backup/nvim/lua/config/keymaps.lua
+++ extras-backup/nvim/lua/config/keymaps.lua
@@ -4,7 +4,8 @@
 
 -- <cmd> or : is command mode in an action
 -- Some various shifts possibly free
--- Alt sends an escape prefix
+-- Alt sends an escape prefix but also seems to check modifier state
+-- :map xxx<cr> looks up mapping of a binding to xxx
 
 -- N.B. Builtins use vim.fn prefix
 -- lua_ls LSP is slightly late binding on warning of unsed function name in dict
@@ -14,8 +15,16 @@ local function nkey(seq, desc, action)
   vim.keymap.set("n", seq, action, { desc = desc })
 end
 
+-- also defines for i but ends with n mode
+local function ninkey(seq, desc, action)
+  nkey(seq, desc, action)
+  vim.keymap.set("i", seq, "<esc>" .. action, { desc = desc })
+end
+
+-- remains in mode i if in i
 local function nikey(seq, desc, action)
-  vim.keymap.set("n", seq, action, { desc = desc })
+  -- can't rely on control codes below being nothing in n mode
+  nkey(seq, desc, action)
   -- escape for one action step to normal mode
   vim.keymap.set("i", seq, "<C-\\><C-O>" .. action, { desc = desc })
 end
@@ -25,15 +34,15 @@ end
 -- ABCDEFGHIJKLMNOPQRSTUVWXYZ
 nkey("\\a", "", "")
 
--- Leader Space (Many used, see use by pressing <space> in normal mode)
+-- Leader Space (Many used, see used by pressing <space> in normal mode)
 -- adijkmnopvyz
 -- ABCFGIJMNOPQRSTUVWXYZ
 nkey("<Leader>r", "Open Rofi Combi", ":!rofi -show combi<cr>")
 nkey("<Leader>t", "Terminal", ":term<cr>i")
 
 -- Control
--- Can be in insert mode as wrapped <esc> .. i by <C-\><C-O>
+-- Can be in insert mode as wrapped <esc> .. i by <C-\><C-O> or just <esc>
 -- Perculiar shift combination needed singleton
-nikey("<C-_>", "", "")
+ninkey("<C-_>", "", "")
 -- ABCDEFGHIJKLMNOPQRSTUVXYZ
-nikey("<C-W>", "Write Quick", ":w<cr>")
+nikey("<C-W>", "Write Quick All", ":wall<cr>")
