95b13be722b9f37493b1bf3fff3b2e99b48ff276
jackokring <jackokring@gmail.com>
2024-09-30 15:52:23 +0000

    Monday 2024-09-30 15:52:23

--- extras-backup/nvim/lua/plugins/doris.lua
+++ extras-backup/nvim/lua/plugins/doris.lua
@@ -1,5 +1,29 @@
 -- doris plugin loader for nvim
-return { 
-	"jackokring/doris.nvim", 
-	opts = {}
+return {
+  "jackokring/doris.nvim",
+  -- **dependancies**
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- **option overrides, implies calling .setup(opts)**
+  opts = {},
+  -- **on load build command**
+  -- build = "",
+  -- **on event**
+  -- event = { "BufEnter", "BufEnter *.lua" },
+  -- **on command use**
+  -- cmd = { "cmd" },
+  -- **on filetype**
+  -- ft = { "lua" },
+  -- **on keys**
+  -- keys = {
+  --    -- key tables
+  --    {
+  --      "<leader>ft",
+  --      -- "<cmd>Neotree toggle<cr>",
+  --      -- desc = "NeoTree",
+  --      -- mode = "n",
+  --      -- ft = "lua"
+  --    },
+  --  },
 }
